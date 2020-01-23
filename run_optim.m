998f6da8cad18c5cc64a3fe1a32f0e0b1c53a142
Thomas Guillod <guillod@otvam.ch>
2020-01-23 14:19:51 +0100

    Brute force solver is now working

--- run_optim.m
+++ run_optim.m
@@ -4,6 +4,9 @@ close('all')
 addpath(genpath('src'))
 
 %% optim
+param = get_data('bruteforce');
+data_brute = get_optim('bruteforce', param);
+
 param = get_data('ga');
 data_ga = get_optim('ga', param);
 
@@ -11,12 +14,12 @@ param = get_data('gamultiobj');
 data_gamultiobj = get_optim('gamultiobj', param);
 
 %% plot
-fprintf('%.3f / %.3f\n', data_ga.sol.y_1, data_ga.sol.y_2)
+fprintf('%.3f / %.3f\n', data_ga.sol.sol.y_1, data_ga.sol.sol.y_2)
 
 figure()
-plot(data_brute.sol.y_1, data_brute.sol.y_2, 'xb')
+plot(data_brute.sol.sol.y_1, data_brute.sol.sol.y_2, 'xb')
 hold('on')
-plot(data_gamultiobj.sol.y_1, data_gamultiobj.sol.y_2, 'dr')
+plot(data_gamultiobj.sol.sol.y_1, data_gamultiobj.sol.sol.y_2, 'dr')
 grid('on')
 xlabel('y_{1}')
 ylabel('y_{2}')
